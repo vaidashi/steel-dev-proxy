9d995578bf8fdd61f37bab912cd33c1dcdd8c337
vaidashi <vaid50@yahoo.com>
2025-01-20 14:44:13 -0600

    scaffold project

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MetricsModule } from './metrics/metrics.module';
+
+@Module({
+  imports: [MetricsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
